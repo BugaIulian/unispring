a4cbbec0230734758e2e5514329e57649731b41d
BugaIulian <iulian.ionut.buga@gmail.com>
2023-09-13 19:59:41 +0300

    Add Student.java entity for JPA

---
+++ src/main/java/com/unispring/demo/models/entities/Student.java
@@ -0,0 +1,26 @@
+package com.unispring.demo.models.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @Column
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "second_name")
+    private String secondName;
+
+    @Column(name = "email")
+    private String studentEmail;
+}
