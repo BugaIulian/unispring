f4703dd0c21f1ac8687417ca5705eda0659c252d
BugaIulian <iulian.ionut.buga@gmail.com>
2023-09-14 15:10:06 +0300

    Add package-info.java to student DTO and student entity

---
+++ src/main/java/com/unispring/demo/models/dto/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This package contains Data Transfer Objects (DTOs) for the UniSpring application.
+ * DTOs are used to transfer data between different application layers.
+ *
+ * @author iulianbuga
+ * @version 1.0
+ * @since 1.0
+ */
+package com.unispring.demo.models.dto;
