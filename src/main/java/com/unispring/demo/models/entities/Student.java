b6bd07df6863e0e7692517d3c1c04cb40c3ebe56
BugaIulian <iulian.ionut.buga@gmail.com>
2023-09-14 14:42:01 +0300

    Add Javadoc to Student class

--- src/main/java/com/unispring/demo/models/entities/Student.java
+++ src/main/java/com/unispring/demo/models/entities/Student.java
@@ -6,21 +6,37 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
+/**
+ * Student class represents a student entity in the system.
+ * It is annotated with JPA annotations to map it to a database table.
+ */
 @Data
 @Entity
 @Table(name = "students")
 public class Student {
 
+    /**
+     * Unique identifier for a the student.
+     */
     @Id
     @Column
     private long id;
 
+    /**
+     * First name of the student.
+     */
     @Column(name = "first_name")
     private String firstName;
 
+    /**
+     * Second name of the student.
+     */
     @Column(name = "second_name")
     private String secondName;
 
+    /**
+     * Email address of the student.
+     */
     @Column(name = "email")
     private String studentEmail;
 }
