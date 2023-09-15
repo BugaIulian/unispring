e0d42ce6cbaa7ad32b0914093645425b09d6c59e
BugaIulian <iulian.ionut.buga@gmail.com>
2023-09-15 10:25:15 +0300

    Add package-info.java to main class and Java doc to the main class

---
+++ src/main/java/com/unispring/demo/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * This package is the main entry point for the UniSpring app.
+ * <p>
+ * It contains the Spring Boot application class to bootstrap the application.
+ * </p>
+ *
+ * @author iulianbuga
+ * @version 1.0
+ * @since 1.0
+ */
+package com.unispring.demo;
